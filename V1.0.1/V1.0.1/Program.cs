9730b5cba392272b0c3fe2b3482ffb71f6e9de74
Valentin Hervouet <valentin.hervouet@skema.edu>
2022-12-08 13:13:37 +0100

    Updated menu

--- V1.0.1/V1.0.1/Program.cs
+++ V1.0.1/V1.0.1/Program.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using V1._0._1;
 
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         // We open the app menu
         Console.WriteLine("EasySave 1.0.1\n");
+        Console.WriteLine("\r\n _____                  _____                 \r\n|  ___|                /  ___|                \r\n| |__  __ _ ___ _   _  \\ `--.  __ ___   _____ \r\n|  __|/ _` / __| | | |  `--. \\/ _` \\ \\ / / _ \\\r\n| |__| (_| \\__ \\ |_| | /\\__/ / (_| |\\ V /  __/\r\n\\____/\\__,_|___/\\__, | \\____/ \\__,_| \\_/ \\___|\r\n                 __/ |                        \r\n                |___/                         \r\n");
+        Console.WriteLine("Make a choice :\n\nCreateNewSave : To create a new save\nSave : To list existing save\nShowDailyLog : To Show daily log\nShowStateLog : To show state log\nExit : To exit\n");
+        Console.Title = "EasySave 1.0.1";
 
         // We let the user make a choice
-        string MenuChoice = null;
+        string MenuChoice;
+        string MenuChoiceLower;
+        bool vrai = true;
 
-        while (MenuChoice != "exit")
+        while (vrai)
         {
-
-
-            Console.WriteLine("Make a choice :\n\nCreateNewSave : To create a new save\nSave : To list existing save\nShowDailyLog : To Show daily log\nShowStateLog : To show state log\nExit : To exit\n");
-
             MenuChoice = Console.ReadLine();
+            MenuChoiceLower = MenuChoice.ToLower();
 
             /// We prompt the user for a string
-            switch (MenuChoice)
+            switch (MenuChoiceLower)
             {
                 // Choice 1 : create a new save
-                case "CreateNewSave":
+                case "createnewsave":
 
                     // Sub choice 1 : choose folder to copy
 
@@ -39,7 +42,7 @@ internal class Program
                     break;
 
                 // Choice 2 : save
-                case "Save":
+                case "save":
 
                     // Sub choice 1 : save 1 save
 
@@ -47,15 +50,21 @@ internal class Program
                     break;
 
                 // Choice 3 : show daily log
-                case "ShowDailyLog":
+                case "showdailylog":
                     break;
 
                 // Choice 4 : show state log
-                case "ShowStateLog":
+                case "showstatelog":
                     break;
 
                 // Choice 5 : exit
-                case "Exit":
+                case "exit" :
+                    vrai = false;
+                    break;
+
+                // In case the user type something else
+                default:
+                    Console.WriteLine("\nType again \n");
                     break;
             }
         }
